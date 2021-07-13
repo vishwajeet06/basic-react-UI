89119207af4cbd89b73cc74c58dfde0d36086c31
vishwajeet06 <vishwajeetkcse@gmail.com>
2021-07-13 13:09:06 +0530

    assignmentD1

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ ButtonText }) => {
+  return <button className="button"> {ButtonText} </button>;
+};
+
+export default Button;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Main = () => {
+  return(
+  <div>
+    <h1 className="main">Hello Everyone</h1>
+  </div>);
+};
+
+export default Main;
